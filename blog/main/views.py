15fcd74773c8cf0a5658ad07e9dd392b45827e3d
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-10-16 06:53:41 +0300

    feat(User): add user's creation endpoint

--- blog/main/views.py
+++ blog/main/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ViewSet):
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        permission_classes = [permissions.AllowAny]
+        return [permission() for permission in permission_classes]
+
+    def create(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
