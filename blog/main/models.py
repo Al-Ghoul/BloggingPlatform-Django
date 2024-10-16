20d3cf079fb96ef13cfd1f95c33f5b0dc0978fb9
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-10-16 04:14:02 +0300

    feat(models): add initial user's models

--- blog/main/models.py
+++ blog/main/models.py
@@ -1,1 +1,21 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.utils.translation import gettext as _
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(_("Email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+
+    objects = UserManager()
